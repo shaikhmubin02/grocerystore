8743b6422bea1e5918091ab431b9812688dc3e5b
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-07-20 14:57:32 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Homepage } from "@/components/component/homepage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
